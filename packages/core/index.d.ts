03e7f12f3d57b7177d8b6a89346c811c7a5dd809
Đặng Hùng <danghungtb26@gmail.com>
2021-01-18 15:25:55 +0700

    chore: remove file

--- packages/core/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { default as debounce } from './debounce';
-export { default as formatMoneyVnd } from './formatMoneyVnd';
-export { default as removeVietnamese } from './removeVietnamese';
-export * from './hookcustom';
-export { default as regex } from './regex';
-export { default as validator } from './validator';
-export * from './api';
