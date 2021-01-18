527491a9b441a5c9e315b9eda295e6d2679cab63
Đặng Hùng <danghungtb26@gmail.com>
2021-01-18 15:25:05 +0700

    chore: any

---
+++ packages/core/index.d.ts
@@ -0,0 +1,7 @@
+export { default as debounce } from './debounce';
+export { default as formatMoneyVnd } from './formatMoneyVnd';
+export { default as removeVietnamese } from './removeVietnamese';
+export * from './hookcustom';
+export { default as regex } from './regex';
+export { default as validator } from './validator';
+export * from './api';
